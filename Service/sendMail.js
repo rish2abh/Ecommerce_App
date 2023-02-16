145679a5711950e2b78c7fd65c956bb8ac8aff26
rish2abh <117706596+rish2abh@users.noreply.github.com>
2023-02-16 15:50:58 +0530

    Update sendMail.js

--- Service/sendMail.js
+++ Service/sendMail.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
     service:'gmail',
     auth:{
         user:"rishabhshrivastava205@gmail.com",
-        pass:"ppgwmtkrfihspuqn"
+        pass:"**************"
 
     }
 });
